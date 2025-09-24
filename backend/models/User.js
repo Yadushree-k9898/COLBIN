b0e600a2201a4d5a1848de002bf3d897d9662107
yadu <kyadushree47@gmail.com>
2025-09-24 17:40:45 +0530

    bug-fix

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: [true, 'Name is required']},
-    email: {type: String, required: [type, 'Email is required'], unique: true},
+    email: {type: String, required: [true, 'Email is required'], unique: true},
     password: {type: String, required: [true, 'Password is required']},
     createdAt: {type:Date, default: Date.now}
 })
